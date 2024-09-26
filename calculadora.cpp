12547f79b4552ec1aeec327bafdbda1b33481a7a
lamejoruniversidadupb <gustavo.nunez@upb.edu.co>
2024-09-26 13:04:31 -0500

    parte suma

---
+++ calculadora.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    double num1, num2, suma;
+
+    cout << "Ingrese el primer número: ";
+    cin >> num1;
+    cout << "Ingrese el segundo número: ";
+    cin >> num2;
+
+    suma = num1 + num2;
+
+
+    cout << "La suma de " << num1 << " y " << num2 << " es: " << suma << endl;
+
+    return 0;
+}
